aceb0a26ea613c9a04f73a847bec36f1dfdc09ec
wm4 <wm4@nowhere>
2018-09-14 00:50:09 +0200

    strtod_test: make it roundtrip all around
    This test sort of tests both snprintf() float formatting and strtod()
    float parsing. It seems justified to first test whether the system
    function roundtrip on their own at all, and then test the libinsanity
    functions. Further, it would be nice to test both lin_snprintf() and
    lin_strtod(), so test them each with the (assumed to work) system
    functions. We don't need to test lin_snprintf+lin_strtod, because they
    would use exactly the same numbers and strings as the other tests.

--- tests/strtod_test.c
+++ tests/strtod_test.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <math.h>
 
+#include "printf.h"
 #include "strtod.h"
 
 /* r = place to store result
@@ -22,7 +23,11 @@
 ((r) = (f)) == (x) || \
 (printf(__FILE__ ":%d: %s failed (" m ")\n", __LINE__, #f, r, x), err++, 0) )
 
-int main()
+__attribute__((format(printf, 3, 4)))
+typedef int (*snprintf_type)(char *str, size_t size, const char *format, ...);
+typedef double (*strtod_type)(const char *nptr, char **endptr);
+
+static int run_test(snprintf_type cur_snprintf, strtod_type cur_strtod)
 {
 	int i;
 	double d, d2;
@@ -31,15 +36,33 @@ int main()
 
 	for (i=0; i<100; i++) {
 		d = sin(i);
-		snprintf(buf, sizeof buf, "%.300f", d);
-		TEST3(d2, lin_strtod(buf, 0), d, "round trip fail %a != %a (%a)");
+		cur_snprintf(buf, sizeof buf, "%.300f", d);
+		TEST3(d2, cur_strtod(buf, 0), d, "round trip fail %a != %a (%a)");
 	}
 
-	TEST2(d, lin_strtod("0x1p4", 0), 16.0, "hex float %a != %a");
-	TEST2(d, lin_strtod("0x1.1p4", 0), 17.0, "hex float %a != %a");
+	TEST2(d, cur_strtod("0x1p4", 0), 16.0, "hex float %a != %a");
+	TEST2(d, cur_strtod("0x1.1p4", 0), 17.0, "hex float %a != %a");
 
 	if (!err)
-		printf("All tests succeeded.\n");
+		printf("Tests succeeded.\n");
 
+    return err;
+}
+
+int main()
+{
+    int err = 0;
+
+    printf("Testing system snprintf() + system strtod()...\n");
+    err |= run_test(snprintf, strtod);
+
+    printf("Testing system snprintf() + lin_strtod()...\n");
+    err |= run_test(snprintf, lin_strtod);
+
+    printf("Testing lin_snprintf() + system strtod()...\n");
+    err |= run_test(lin_snprintf, strtod);
+
+    if (!err)
+		printf("All tests succeeded.\n");
 	return err ? 1 : 0;
 }
